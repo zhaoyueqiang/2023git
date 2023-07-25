1556fdd6f368cab18cd938aed2f494a2c83a9c13
zhaoyueqiang <2415486724@qq.com>
2023-07-25 19:02:40 +0800

    helloworld

---
+++ CudaRuntime1/kernel.cu
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+__global__ void hello_world(void)
+{
+	printf("GPU: Hello world!\n"); // GPU输出的Hello World!
+}
+int main(int argc, char** argv)
+{
+	printf("CPU: Hello world!\n"); // CPU输出的Hello World!
+	hello_world << <1, 10 >> > ();
+	cudaDeviceReset();             // 如果没有这一行就看不到GPU输出的Hello World!
+	return 0;
+}
